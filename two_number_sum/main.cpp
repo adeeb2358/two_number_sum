e2036b9dd3e88e41f3cb52f942b3fc62ee263f97
Adeeb Mohammed <adeeb2358@gmail.com>
2019-11-30 20:54:26 +0900

    Initial Commit

---
+++ two_number_sum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  two_number_sum
+//
+//  Created by adeeb mohammed on 30/11/19.
+//  Copyright © 2019 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
